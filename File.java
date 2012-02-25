4905fca4189a8e52106df04c853ca85d8d1e50b5
kennethv <verstraetekenneth@gmail.com>
2012-02-25 12:16:41 +0100

    Update File.java

--- File.java
+++ File.java
@@ -1,4 +1,12 @@
 import be.kuleuven.cs.som.annotate.*;
+
+/**
+ * A class of files with a name and size
+ * @invar There always has to be a name for a file, the name can only consist of: 
+ *        lower- or uppercase letters, numbers, dots (.), hyphens (-) and underscores (_).
+ * @author Pieterjan Lambrecht and Kenneth Verstraete
+ *
+ */
 public class File {
 	public File(){
 		
